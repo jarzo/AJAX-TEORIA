b1268314123796cfad954ea482514ac2b24a1ba9
Michał Jarząbek <nebraska994@gmail.com>
2017-06-25 13:37:04 +0200

    next

--- 1-ajax/js/ajax-skonczona-wersja.js
+++ 1-ajax/js/ajax-skonczona-wersja.js
@@ -3,24 +3,38 @@
 //Tworzymy funkcjie
 function pobierzDane() {
 	
-	var httpReq = new XMLHttpRequest; // tworzymy obiekt (kupujemy telefon)
+	var httpReq = new XMLHttpRequest(); // tworzymy obiekt (kupujemy telefon)
 	
-	httpReq.open("GET", "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", true);// otwieramy połączenie z serwerem (nawiązujemy połączenie) definiujemy gdzie wysyłamy
+	httpReq.open("GET",
+				 "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", true);// otwieramy połączenie z serwerem (nawiązujemy połączenie) definiujemy gdzie wysyłamy
 	
 	//Tworzymy funkcjia w której, definiuje co będzie się wydarzyć gdy serwer odpowie
 	httpReq.onreadystatechange = function () {
 		
-		console.log(http.readyState);
-		
 		//Gdy jest równy 4, jak dane są gotowe to ich status zmienia się na 4
 		if (httpReq.readyState == 4) {
+			console.log(httpReq.status);
 			
-			httpReq = null;
+			if(httpReq.status >=200 && httpReq.status <300 || httpReq.status == 304 || navigator.userAgent.indexOf("Safari") >= 0 && httpReq.status == "undefined"){
+			
+				console.log(httpReq.responseText);
+				var returnData = JSON.parse(httpReq.responseText);
+				
+				console.log(returnData);
+				
+				var userName = document.createElement("p");
+				userName.innerHTML = "Nazwa użytkownika to:" + returnData.userName;
+				document.body.appendChild(userName);
+				
+				console.log(userName);
+				
+				httpReq = null;//nulem przerywamy połączenie
+			}
 		}
 	}
 	
 	//Wysyłamy, połącz z serwerem/iniciowanie połaczenia
-	httpReq.send
+	httpReq.send();
 	
 }
 
